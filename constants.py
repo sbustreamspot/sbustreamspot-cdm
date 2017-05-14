95ea1e45a363a6a266ae7d33bc895bea882f0a40
darpa <darpa@ta2-marple-ss.tc.bbn.com>
2017-05-14 18:41:55 -0400

    update types for CDM17

--- constants.py
+++ constants.py
@@ -8,27 +8,27 @@ KAFKA_URL = 'ta3.tc.dev:9092'
 SCHEMA_FILE = 'schema/TCCDMDatum.avsc'
 
 # CDM record type constants
-CDM_TYPE_PRINCIPAL = 'com.bbn.tc.schema.avro.Principal'
-CDM_TYPE_SUBJECT = 'com.bbn.tc.schema.avro.Subject'
-CDM_TYPE_FILE = 'com.bbn.tc.schema.avro.FileObject'
-CDM_TYPE_MEM = 'com.bbn.tc.schema.avro.MemoryObject'
-CDM_TYPE_SOCK = 'com.bbn.tc.schema.avro.NetFlowObject'
-CDM_TYPE_EDGE = 'com.bbn.tc.schema.avro.SimpleEdge'
-CDM_TYPE_EVENT = 'com.bbn.tc.schema.avro.Event'
-CDM_TYPE_SRCSINK = 'com.bbn.tc.schema.avro.SrcSinkObject'
+CDM_TYPE_PRINCIPAL = 'Principal'
+CDM_TYPE_SUBJECT = 'Subject'
+CDM_TYPE_FILE = 'FileObject'
+CDM_TYPE_MEM = 'MemoryObject'
+CDM_TYPE_SOCK = 'NetFlowObject'
+CDM_TYPE_SRCSINK = 'SrcSinkObject'
+CDM_TYPE_EDGE = 'SimpleEdge'
+CDM_TYPE_EVENT = 'Event'
 
 type_map = {
     # SubjectType
     'SUBJECT_PROCESS': 'a',
-    #'SUBJECT_THREAD': 'b',
-    #'SUBJECT_UNIT': 'c',
-    #'SUBJECT_BASIC_BLOCK': 'd'
+    'SUBJECT_THREAD': 'b',
+    'SUBJECT_UNIT': 'c',
+    'SUBJECT_BASIC_BLOCK': 'd',
 
     # Object Type
-    'OBJECT_FILE': 'e',
-    'OBJECT_MEM': 'f',
-    'OBJECT_SOCK': 'g',
-    'OBJECT_SRCSINK': 'O', # Added CDM13
+    'FILE_OBJECT_FILE': 'e',
+    'FILE_OBJECT_DIR': 'e', # same as file
+    'FILE_OBJECT_NAMED_PIPE': 'f', # CDM17
+    'FILE_OBJECT_UNIX_SOCKET': 'g', # CDM17
 
     # EventType
     'EVENT_ACCEPT': 'h',
@@ -56,12 +56,38 @@ type_map = {
     'EVENT_SIGNAL': 'D',
     'EVENT_TRUNCATE': 'E',
     'EVENT_WAIT': 'F',
-    'EVENT_OS_UNKNOWN': 'G',
-    'EVENT_KERNEL_UNKNOWN': 'H',
-    'EVENT_APP_UNKNOWN': 'I',
-    'EVENT_UI_UNKNOWN': 'J',
-    'EVENT_UNKNOWN': 'K',
+    #'EVENT_OS_UNKNOWN': 'G',
+    #'EVENT_KERNEL_UNKNOWN': 'H',
+    #'EVENT_APP_UNKNOWN': 'I',
+    #'EVENT_UI_UNKNOWN': 'J',
+    'EVENT_MOUNT': 'G',
+    'EVENT_STARTSERVICE': 'H',
+    'EVENT_LOGIN': 'I',
+    'EVENT_LOGOUT': 'J',
+    #'EVENT_UNKNOWN': 'K',
     'EVENT_BLIND': 'L',
     'EVENT_UNIT': 'M',
     'EVENT_UPDATE': 'N',
+    
+    'FILE_OBJECT_PEFILE': 'O', # Added CDM13
+    
+    # CDM17
+    'EVENT_DUP': 'P',
+    'EVENT_FNCTL': 'Q',
+    'EVENT_LSEEK': 'R',
+    'EVENT_OTHER': 'S',
+    'EVENT_SENDTO': 'T',
+    'EVENT_SENDMSG': 'U',
+    'EVENT_SHM': 'V',
+    'EVENT_EXIT': 'W',
+    'EVENT_LOADLIBRARY': 'X',
+    'EVENT_BOOT': 'Y',
+    'EVENT_LOGCLEAR': 'Z',
+
+    # objects without subtypes
+    'UnnamedPipeObject': '1',
+    'RegistryKeyObject': '2',
+    'NetFlowObject': '3',
+    'MemoryObject': '4',
+    'SrcSinkObject': '5', # subtypes exist but are Android-specific, except IPC
 }
